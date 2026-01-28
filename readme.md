08faeb0dc44df60a912ebc56178f231776e03103
joerivanveen <nonstockphoto@gmail.com>
2026-01-28 11:20:41 +0100

    feat: add option to add current domain as suffix to iframe src

--- readme.md
+++ readme.md
@@ -4,4 +4,10 @@ Lightweight companion for the full [Ruigehond embed](https://github.com/joerivan
 
 When enabled, use the shortcode \[ruigehond-embed-parent src="&lt;Iframe src&gt;"] to embed urls from sites where the full version of ruigehond-embed is installed.
 
-This code is also contained in the regular Ruigehond embed plugin.
+Note: if you add `domain="any value"` to the shortcode it will _add the domain_ it is currently on to the iframe src, in the form of a slug.
+
+Example: if you put `https://parent-site.com/test` in the src attribute, and you are visiting from ‘child-site.com’, the actual iframe src will be `https://parent-site.com/test-child-site-com`.
+
+Subdomains will be passed as well, except www.
+
+Tip: you can always inspect the iframe to see what is exactly requested, and adapt your shortcode or main site to that.
--- ruigehond-embed-parent/ruigehond-embed-parent.php
+++ ruigehond-embed-parent/ruigehond-embed-parent.php
@@ -4,7 +4,7 @@ declare( strict_types=1 );
 Plugin Name: Ruigehond embed parent
 Plugin URI: https://github.com/joerivanveen/ruigehond-embed
 Description: When enabled, use the shortcode [ruigehond-embed-parent src="&lt;Iframe src&gt;"] to embed urls from sites where the full version of ruigehond-embed is installed.
-Version: 1.2.20
+Version: 1.3.0
 Requires at least: 5.0
 Tested up to: 6.5
 Requires PHP: 7.4
@@ -28,6 +28,20 @@ function ruigehond_embed_parent_shortcode( $attributes = [], $content = null, $s
 		return "Ruigehond embed: src not recognized as a valid iframe src. Use a fully qualified url.";
 	}
 	wp_enqueue_script( 'ruigehond015_snuggle_javascript', plugin_dir_url( __FILE__ ) . 'snuggle.js', [], '1.2.20' );
+
+	// add the current domain to the iframe src slug
+	if ( isset($attributes['domain']) && true === isset( $_SERVER['HTTP_HOST'] ) ) {
+		// https://stackoverflow.com/questions/7111881/what-are-the-allowed-characters-in-a-subdomain
+		$domain = str_replace( '.', '-', trim( preg_replace( '/[^a-z0-9\-\.]/', '', strtolower( wp_unslash( $_SERVER['HTTP_HOST'] ) ) ), '-.' ) );
+		if ( 0 === strpos( $domain, 'www-' ) ) {
+			$domain = substr( $domain, 4 );
+		}
+		if ('/' === substr($src, -1) ) {
+			$src = "$src$domain";
+		} else {
+			$src = "$src-$domain";
+		}
+	}
 
 	return "<iframe style='width:100%;border:0;frame-border:0;height:100vh;' loading='eager' src='$src'></iframe>";
 }
